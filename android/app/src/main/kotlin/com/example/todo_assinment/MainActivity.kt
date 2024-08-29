10caecf7d68b8822479ba12ef3ac0403e46fb7e5
mina <talaatmina195@gmail.comj>
2024-08-29 05:22:26 +0300

    Initial commit splash_screen

---
+++ android/app/src/main/kotlin/com/example/todo_assinment/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.todo_assinment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppTheme{
+  static ThemeData lightTheme = ThemeData(
+    scaffoldBackgroundColor: Color(0xffDFECDB),
+  );
+}
